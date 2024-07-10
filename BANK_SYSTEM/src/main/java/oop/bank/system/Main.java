6b387137600f9532218cba4435e74f737bdf91e0
milamailo <miila.maiilo@gmail.com>
2024-07-10 00:34:56 -0400

    CheckingAccount class tested

--- BANK_SYSTEM/src/main/java/oop/bank/system/Main.java
+++ BANK_SYSTEM/src/main/java/oop/bank/system/Main.java
@@ -9,6 +9,8 @@ public class Main {
         double annualFee = 100.0;
         long accountNumber = (long) (Math.random() * 9999999999L + 1000000000L);
         double interestRate = 1.5;
+        double overdraftLimit = 500.0;
+
         List<String> questions = new ArrayList<>();
         questions.add("Please enter your First Name");
         questions.add("Please enter your Last Name");
@@ -29,42 +31,37 @@ public class Main {
         }
         String accountHolder = accountHolderBuilder.toString();
 
-        // Create a SavingsAccount object
-        SavingsAccount savingsAccount = new SavingsAccount(accountNumber, accountHolder, interestRate, annualFee);
+        // Create a CheckingAccount object
+        CheckingAccount checkingAccount = new CheckingAccount(accountNumber, accountHolder, overdraftLimit, annualFee);
 
         // Print initial balance and account details
-        System.out.println("Initial balance: " + savingsAccount.getBalance());
-        System.out.println("Account type: " + savingsAccount.getAccountType());
-        savingsAccount.accountSummary();
+        System.out.println("Initial balance: " + checkingAccount.getBalance());
+        checkingAccount.accountSummary();
 
         // Test deposit method
-        boolean depositSuccess = savingsAccount.deposit(200.0);
+        boolean depositSuccess = checkingAccount.deposit(200.0);
         System.out.println("Deposit successful: " + depositSuccess);
-        System.out.println("Balance after deposit: " + savingsAccount.getBalance());
+        System.out.println("Balance after deposit: " + checkingAccount.getBalance());
 
-        // Test withdraw method
+        // Test withdraw method within overdraft limit
         try {
-            boolean withdrawSuccess = savingsAccount.withdraw(100.0);
+            boolean withdrawSuccess = checkingAccount.withdraw(600.0); // Test overdraft
             System.out.println("Withdraw successful: " + withdrawSuccess);
-            System.out.println("Balance after withdrawal: " + savingsAccount.getBalance());
+            System.out.println("Balance after withdrawal: " + checkingAccount.getBalance());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
         // Test withdraw method with insufficient funds
         try {
-            boolean withdrawSuccess = savingsAccount.withdraw(200.0);
+            boolean withdrawSuccess = checkingAccount.withdraw(1000.0); // Exceeds overdraft limit
             System.out.println("Withdraw successful: " + withdrawSuccess);
-            System.out.println("Balance after withdrawal: " + savingsAccount.getBalance());
+            System.out.println("Balance after withdrawal: " + checkingAccount.getBalance());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
-        // Add interest
-        savingsAccount.addInterest();
-        System.out.println("Balance after adding interest: " + savingsAccount.getBalance());
-
         // Print account summary
-        savingsAccount.accountSummary();
+        checkingAccount.accountSummary();
     }
 }
