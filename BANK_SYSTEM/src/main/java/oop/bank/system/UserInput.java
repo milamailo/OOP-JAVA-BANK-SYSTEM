c3e49de4cccce0365063cad20615b6949a40abf1
milamailo <miila.maiilo@gmail.com>
2024-07-09 23:34:34 -0400

    BankAccount Class added and tested

--- BANK_SYSTEM/src/main/java/oop/bank/system/UserInput.java
+++ BANK_SYSTEM/src/main/java/oop/bank/system/UserInput.java
@@ -8,12 +8,12 @@ import java.util.Scanner;
 
 public class UserInput {
     private List<String> questions;
-    private Map<String, String> answers;
+    private List<String> answers;
     private Scanner scanner;
 
     public UserInput(List<String> questions) {
         this.questions = new ArrayList<>(questions);
-        this.answers = new HashMap<>();
+        this.answers = new ArrayList<>();
         this.scanner = new Scanner(System.in);
     }
 
@@ -21,7 +21,7 @@ public class UserInput {
         for (String question : questions) {
             System.out.print(String.format("%s: ", question));
             String answer = scanner.nextLine();
-            answers.put(question, answer);
+            answers.add(answer);
         }
     }
 
@@ -33,7 +33,16 @@ public class UserInput {
         this.questions = new ArrayList<>(questions);
     }
 
-    public Map<String, String> getAnswers() {
-        return this.answers;
+    public List<String> getAnswers() {
+        List answers = new ArrayList<String>();
+
+        return this.questions;
+    }
+    public Map<String, String> getQuestionsAndAnswers() {
+        Map<String, String> questionsAndAnswers = new HashMap<>();
+        for (int i = 0; i < questions.size(); i++) {
+            questionsAndAnswers.put(questions.get(i), answers.get(i));
+        }
+        return questionsAndAnswers;
     }
 }
